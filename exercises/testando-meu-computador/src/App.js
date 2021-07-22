86bc7713e94e17704c09039d717dc1fb9897a138
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-07-22 18:20:53 -0300

    Enviando os exercicios do bloco1 dia 1

---
+++ exercises/testando-meu-computador/src/App.js
@@ -0,0 +1,17 @@
+import Component from "./Component";
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+
+function App() {
+  return ( 
+    <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+  );
+}
+
+export default App;
